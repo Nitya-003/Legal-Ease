2e23d5a548ee6ccf50a17ff3ee1078c6cfc91194
Nitya Gosain <nityagosain@gmail.com>
2025-09-23 20:20:15 +0530

    Create page.tsx

---
+++ app/chat/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardLayout } from "@/components/dashboard-layout"
+import { DocumentChat } from "@/components/document-chat"
+
+export default function ChatPage() {
+  return (
+    <DashboardLayout>
+      <DocumentChat />
+    </DashboardLayout>
+  )
+}
