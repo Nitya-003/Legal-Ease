a4552fd6f97186568a3c99d41998516308f4f02a
Nitya Gosain <nityagosain@gmail.com>
2025-09-23 20:15:21 +0530

    Create page.tsx

---
+++ app/simplify/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardLayout } from "@/components/dashboard-layout"
+import { DocumentSimplifier } from "@/components/document-simplifier"
+
+export default function SimplifyPage() {
+  return (
+    <DashboardLayout>
+      <DocumentSimplifier />
+    </DashboardLayout>
+  )
+}
