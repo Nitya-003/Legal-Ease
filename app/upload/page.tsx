f6c398349e12576657f13899bc9bcd7ebb35d363
Nitya Gosain <nityagosain@gmail.com>
2025-09-23 20:14:35 +0530

    Create page.tsx

---
+++ app/upload/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardLayout } from "@/components/dashboard-layout"
+import { DocumentUpload } from "@/components/document-upload"
+
+export default function UploadPage() {
+  return (
+    <DashboardLayout>
+      <DocumentUpload />
+    </DashboardLayout>
+  )
+}
