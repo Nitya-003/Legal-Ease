5b98caf1b73d8ede2cb8de7af96e7fa0c3088d67
Nitya Gosain <nityagosain@gmail.com>
2025-09-23 20:16:03 +0530

    Create page.tsx

---
+++ app/risks/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardLayout } from "@/components/dashboard-layout"
+import { RiskAnalysis } from "@/components/risk-analysis"
+
+export default function RisksPage() {
+  return (
+    <DashboardLayout>
+      <RiskAnalysis />
+    </DashboardLayout>
+  )
+}
