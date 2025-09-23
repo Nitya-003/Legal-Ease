e6c49a7d21f91b2bc37ee2b63c0b9638409bb3ea
Nitya Gosain <nityagosain@gmail.com>
2025-09-23 20:17:26 +0530

    Create page.tsx

---
+++ app/download/page.tsx
@@ -0,0 +1,10 @@
+import { DashboardLayout } from "@/components/dashboard-layout"
+import { ExportCenter } from "@/components/export-center"
+
+export default function DownloadPage() {
+  return (
+    <DashboardLayout>
+      <ExportCenter />
+    </DashboardLayout>
+  )
+}
